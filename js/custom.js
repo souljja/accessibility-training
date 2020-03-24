d1eb431a42fa6565593d3c04023bf8e07afa2ab3
Андрей Богородицкий <andrejjbogorodickijj@gmail.com>
2020-03-24 03:30:01 +0300

    added aria-labels and delegation

--- js/custom.js
+++ js/custom.js
@@ -12,54 +12,53 @@ var ARROW_DOWN_KEY = 40;
 var ARROW_RIGHT_KEY = 39;
 var ARROW_LEFT_KEY = 37;
 
-document.querySelectorAll("#nav li").forEach(function (navEl, _, parent) {
-  navEl.onclick = function () {
-    toggleTab(this.id, this.dataset.target);
-  };
-  navEl.onkeydown = function (event) {
-    var id = parseInt(this.id);
-    var target;
-    var resultId;
+var tabMenu = document.getElementById("nav");
+var liElements = document.querySelectorAll("#nav li");
+tabMenu.onclick = function (event) {
+  var listElement = event.target.closest("[data-target]");
+  if (listElement) {
+    toggleTab(listElement.id, listElement.dataset.target);
+  }
+}
+
+tabMenu.onkeydown = function (event) {
+  var listElement = event.target.closest("[data-target]");
+  if (listElement) {
+    var id = parseInt(listElement.id);
+    var liElementsLength = liElements.length;
     switch (event.keyCode) {
       case ARROW_DOWN_KEY:
-      case ARROW_LEFT_KEY: {   
-        event.preventDefault();  
-        if (id === 1) {
-          resultId = String(parent.length);
-        } else {
-          resultId = String(id - 1);
-        }
-        target = this.dataset.target.replace(this.id, resultId);
-        toggleTab(resultId, target);
+      case ARROW_LEFT_KEY: {
+        event.preventDefault();
+        handleElement(listElement, id === 1 ? String(liElementsLength) : String(id - 1));
         break;
       }
       case ARROW_UP_KEY:
       case ARROW_RIGHT_KEY: {
         event.preventDefault();
-        if (id === parent.length) {
-          resultId = '1';
-        } else {
-          resultId = String(id + 1);
-        }
-        target = this.dataset.target.replace(this.id, resultId);
-        toggleTab(resultId, target);
+        handleElement(listElement, id === liElementsLength ? '1' : String(id + 1));
         break;
       }
     }
   }
-});
+}
+
+function handleElement(element, newId) {
+  var target = element.dataset.target.replace(element.id, newId);
+  toggleTab(newId, target);
+}
 
 function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
-
-  navEls.forEach(function (navEl) {
+  liElements.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
       navEl.setAttribute('tabindex', '0');
+      navEl.setAttribute('aria-selected', true);
       navEl.focus();
     } else if (navEl.classList.contains("is-active")) {
       navEl.classList.remove("is-active");
       navEl.setAttribute('tabindex', '-1');
+      navEl.setAttribute('aria-selected', false);
     }
   });
 
