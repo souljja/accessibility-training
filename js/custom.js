f5b7081e00af74af52c300b5c6d88d6a5f2d7cb6
Андрей Богородицкий <andrejjbogorodickijj@gmail.com>
2020-04-10 01:15:12 +0300

    replaced keyCode with code

--- js/custom.js
+++ js/custom.js
@@ -17,10 +17,10 @@ setInterval(function () {
   }
 }, 1000 * 60);
 
-var ARROW_UP_KEY = 38;
-var ARROW_DOWN_KEY = 40;
-var ARROW_RIGHT_KEY = 39;
-var ARROW_LEFT_KEY = 37;
+var ARROW_UP_CODE = 'ArrowUp';
+var ARROW_DOWN_CODE = 'ArrowDown';
+var ARROW_RIGHT_CODE = 'ArrowRight';
+var ARROW_LEFT_CODE = 'ArrowLeft';
 
 var tabMenu = document.getElementById("nav");
 var liElements = document.querySelectorAll("#nav li");
@@ -36,15 +36,15 @@ tabMenu.onkeydown = function (event) {
   if (listElement) {
     var id = parseInt(listElement.id);
     var liElementsLength = liElements.length;
-    switch (event.keyCode) {
-      case ARROW_DOWN_KEY:
-      case ARROW_LEFT_KEY: {
+    switch (event.code) {
+      case ARROW_DOWN_CODE:
+      case ARROW_LEFT_CODE: {
         event.preventDefault();
         handleElement(listElement, id === 1 ? String(liElementsLength) : String(id - 1));
         break;
       }
-      case ARROW_UP_KEY:
-      case ARROW_RIGHT_KEY: {
+      case ARROW_UP_CODE:
+      case ARROW_RIGHT_CODE: {
         event.preventDefault();
         handleElement(listElement, id === liElementsLength ? '1' : String(id + 1));
         break;
